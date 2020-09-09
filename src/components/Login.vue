bf4e8c2daf3a91b09ad6f0e22c9d4577f0dbfb94
laoshama <56189059+laoshama@users.noreply.github.com>
2020-09-09 14:20:00 +0800

    init project OK!

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,7 @@
 <template>
+  <div class="login_box">
 
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,8 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  .login_box {
 
+  }
 </style>
